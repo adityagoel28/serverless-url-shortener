710ac5ef442c5faceb36239be034358d03decada
Aditya Goel <adityagoel2002@gmail.com>
2024-07-06 14:21:50 +0530

    Update providers.tf

--- iac/providers.tf
+++ iac/providers.tf
@@ -1,16 +1,16 @@
-terraform{
-    required_providers{
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 
-    backend "s3"{
-        # This is the backend configuration for the state file
-        bucket = "terraform-url-shortener-state-config"
-        key = "url-shortener/state"
-    }
+  backend "s3" {
+    # This is the backend configuration for the state file
+    bucket = "terraform-url-shortener-state-config"
+    key    = "url-shortener/state"
+  }
 }
 
 provider "aws" {}
